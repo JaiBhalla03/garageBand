4e11da04811bea841a4b2a709b77107898cd248a
jai <jai.bhalla2021@vitstudent.ac.in>
2022-11-16 17:50:39 +0530

    adding background images and link them

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,47 @@
 import {Fragment} from "react";
 import Link from "next/link";
+import Image from "next/image";
+import piano from "../Images/piano.png"
+import drumset from "../Images/drumset.png";
+
 
 export default function Home() {
   return (
-    <Fragment>
-        <title>
-            Garage band
-        </title>
-        <Link href="/piano">
-          Piano
-        </Link>
-        <Link href="/drumset">
-            DrumSet
-        </Link>
-        <div>
-            This is the main page
-        </div>
-    </Fragment>
+      <main className="h-screen p-10">
+          <Fragment>
+              <title>
+                  Garage band
+              </title>
+              <div className="h-full flex flex-col justify-between">
+                  <div>
+                      The Garage Band
+                  </div>
+                  <div className="flex justify-between">
+                      <div className="inline-block hover:scale-125">
+                          <Link href="/drumset">
+                              <Image
+                                  src={drumset}
+                                  width={300}
+                                  height={400}
+                                  alt=""
+                              />
+                          </Link>
+                      </div>
+                      <div className="inline-block hover:scale-125">
+                          <Link href="/piano">
+                              <Image
+                                  src={piano}
+                                  width={300}
+                                  height={300}
+                                  alt=""
+                              />
+                          </Link>
+                      </div>
+                  </div>
+              </div>
+
+          </Fragment>
+      </main>
+
   )
 }
