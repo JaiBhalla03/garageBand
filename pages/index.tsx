0e46dfec418bb9874424471cb48707eab42c04d2
jai <jai.bhalla2021@vitstudent.ac.in>
2022-11-17 15:23:10 +0530

    Adding some more styling and hover effects

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,42 +1,51 @@
-import {Fragment} from "react";
+import {Fragment, useState} from "react";
 import Link from "next/link";
 import Image from "next/image";
 import piano from "../Images/piano.png"
 import drumset from "../Images/drumset.png";
+import {motion} from "framer-motion"
+//import pianoAudio from "../Sounds/piano.wav"
 
 
 export default function Home() {
+
   return (
-      <main className="h-screen p-10">
+      <main className="h-screen pb-4 pt-10 pl-10 pr-10">
           <Fragment>
               <title>
                   Garage band
               </title>
               <div className="h-full flex flex-col justify-between">
-                  <div>
+                  <div className="p-10 font-mono text-blue-300 text-6xl font-extrabold text-shadow-sm">
                       The Garage Band
                   </div>
                   <div className="flex justify-between">
-                      <div className="inline-block hover:scale-125">
+                      <motion.div
+                          whileTap={{scale: 0.75}}
+                          className="relative ease-in-out duration-100 inline-block hover:scale-110">
                           <Link href="/drumset">
                               <Image
                                   src={drumset}
                                   width={300}
-                                  height={400}
+                                  height={300}
                                   alt=""
+                                  className="grayscale hover:grayscale-0"
                               />
                           </Link>
-                      </div>
-                      <div className="inline-block hover:scale-125">
+                      </motion.div>
+                      <motion.div
+                          whileTap={{scale: 0.75}}
+                          className="relative ease-in-out duration-100 inline-block hover:scale-110">
                           <Link href="/piano">
                               <Image
                                   src={piano}
                                   width={300}
                                   height={300}
                                   alt=""
+                                  className="grayscale hover:grayscale-0"
                               />
                           </Link>
-                      </div>
+                      </motion.div>
                   </div>
               </div>
 
